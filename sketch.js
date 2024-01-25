0166e9886ad89282d7fc26f9b80a81d168e6beb2
2Andrew22 <4ndrew2@gmail.com>
2024-01-25 10:45:35 -0500

    update webpage

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let gif;
+
+function preload (){
+gif = loadImage ('assets/TuGIF3.gif');
+
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  strokeWeight(1);
+  fill (87, 94, 138);
+}
+
+function draw() {
+
+background ('white');
+  var num = 10; //# of squares
+  var sideLen = windowWidth/num; //side length of square
+  
+  for (var y = 0; y < windowHeight; y = y + sideLen){
+  for (var x = 0; x < windowWidth; x = x + sideLen){
+    image (gif, x, y, sideLen, sideLen);
+  
+  }
+  }
+
+ 
+  
+}
+ function windowResized (){
+   resizeCanvas (windowWidth, windowHeight);
+ }
