cf494804f27da0dfe2690344b0e0b9a9c40002e5
2Andrew22 <4ndrew2@gmail.com>
2024-01-30 08:53:25 -0500

    messing with sizing

--- sketch.js
+++ sketch.js
@@ -17,8 +17,8 @@ background ('white');
   var num = 10; //# of squares
   var sideLen = windowWidth/num; //side length of square
   
-  for (var y = 0; y < windowHeight; y = y + sideLen){
-  for (var x = 0; x < windowWidth; x = x + sideLen){
+  for (var y = 0; y < 2 * windowHeight; y = y + sideLen){
+  for (var x = 0; x < 2* windowWidth; x = x + sideLen){
     image (gif, x, y, sideLen, sideLen);
   
   }
